017c7de052103cb5da6912e3cdcb87578f167036
Cristian Palermo <103998837+cristianpalermo-bitrocketdev@users.noreply.github.com>
2023-04-03 15:25:54 +0200

    feat: implemented storybook (#33)
    Refs #29

---
+++ .storybook/main.ts
@@ -0,0 +1,6 @@
+/** @format */
+
+module.exports = {
+  stories: ['../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: ['@storybook/addon-actions', '@storybook/addon-links', '@storybook/addon-knobs'],
+};
---
+++ src/components-ui/Button/index.stories.tsx
@@ -0,0 +1,16 @@
+/** @format */
+
+import React from 'react';
+
+import { UIButton } from '.';
+
+const story = {
+  title: 'UIButton',
+  component: UIButton,
+};
+
+export const Disabled = () => <UIButton label="Disabilitato" disabled />;
+
+export const NotDisabled = () => <UIButton label="Abilitato" />;
+
+export default story;
